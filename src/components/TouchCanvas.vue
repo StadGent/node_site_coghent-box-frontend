d2d0e4edf1426eaf02d7caa848c0e8c9e96054aa
Ruben De Swaef <ruben@inuits.eu>
2021-08-05 15:23:24 +0200

    Cleanup

--- src/components/TouchCanvas.vue
+++ src/components/TouchCanvas.vue
@@ -8,8 +8,8 @@ import { defineComponent, onMounted, computed, ref, PropType, Ref, Prop, watch, 
 import { fabric } from 'fabric'
 import * as Hammer from 'hammerjs'
 import { Square } from '../models/SquareModel'
-import { ElasticHit } from '../models/ElasticDataModel'
 import { Result } from '../models/CollectionModel'
+import SetupFabricService, { colorArray } from '../services/canvas/SetupFabricService'
 
 export default defineComponent({
   name: 'TouchCanvas',
@@ -29,70 +29,25 @@ export default defineComponent({
   },
   emits: ['update:basket', 'update:legend'],
   setup: (props, { emit }) => {
+    const fabricService: SetupFabricService = new SetupFabricService()
     let selectedSquare: Square
     let canvas: any
-    const colorArray = ['green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'cyan', 'gold', 'grey', 'maroon', 'olive', 'navy', 'teal']
+    const basketHeight = 130
 
     const bodyWidth = computed<number>(() => {
       return document.body.clientWidth
     })
 
-    const loadRandomSquares = (canvas: any, numOfSquares: number) => {
-      for (let index = 0; index < numOfSquares; index++) {
-        const rect = new fabric.Rect({
-          top : Math.floor(Math.random() * 600) + 100,
-          left : Math.floor(Math.random() * bodyWidth.value) + 100,
-          width : 70,
-          height : 70,
-          fill : colorArray[Math.floor(Math.random() * colorArray.length) + 0],
-          id : 'my-square-' + index
-        })
-
-        canvas.add(rect)
-      }
-    }
-
-    const drawBasket = () => {
-      const basketRect = new fabric.Rect({
-        width : bodyWidth.value - 200,
-        height : 130,
-        fill : 'red',
-        stroke: 'black',
-        strokeWidth: 5,
-        originX: 'center',
-        originY: 'center'
-      })
-
-      const basketText = new fabric.Text('Basket', { 
-        originX: 'center',
-        originY: 'center'
-      })
-
-      const basketSquare = new fabric.Group([ basketRect, basketText ], {
-        top : 0,
-        left : 100,
-        selectable: false,
-        evented: false
-      })
-
-      canvas.add(basketSquare)
-
-      props.basket.forEach((rect) => {
-        canvas.add(rect)
-        canvas.add(rect.title)
-      })
-    }
-
     const setSquareLineOpacity = (square: Square, opacity: number) => {
       square.lines.going.forEach((line: any) => {
-        if(opacity === 100 && line.top < 100){
+        if(opacity === 100 && line.top < basketHeight){
           line.opacity = 0
         } else {
           line.opacity = opacity
         }
       })
       square.lines.coming.forEach((line: any) => {
-        if(opacity === 100 && line.top < 100){
+        if(opacity === 100 && line.top < basketHeight){
           line.opacity = 0
         } else {
           line.opacity = opacity
@@ -102,7 +57,7 @@ export default defineComponent({
 
     const setRect = (rect: Square, entity: Result) => {
       rect.left = fabric.util.getRandomInt(100, bodyWidth.value - 200)
-      rect.top = fabric.util.getRandomInt(130, 600)
+      rect.top = fabric.util.getRandomInt(basketHeight, 600)
       rect.strokeWidth = 5
       rect.id = entity._id,
       rect.data = entity.data,
@@ -121,63 +76,64 @@ export default defineComponent({
     }
 
     const loadEntities = (entities: Result[]) => {
-        canvas.clear()
-        drawBasket()
-        const types = new Map()
-        const legend: any[] = []
-        entities.map((entity) => {
-          fabric.Image.fromURL(entity.image, function(rect: any) {
-            if(entity.image === undefined) {
-              rect = new fabric.Rect({
-                width: 100,
-                height: 100
-              })
-            } else {
-              rect.scaleToHeight(100)
-              //rect.scaleToWidth(100)
-            }
-            setRect(rect, entity)
-            rect.stroke = colorArray[0]
-            canvas.add(rect)
-
-            entity.metadata.map((meta) => {
-              if(meta.key === "type"){
-                const type = types.get(meta.value)
-                if(type){
-                  rect.stroke = type[0].stroke
-                  rect.fill = type[0].stroke
-                  types.set(meta.value, [...type, rect])
-
-                  const entity = type[type.length - 1]
-                  const line = getLine(entity, rect)
-                  entity.lines.going.push(line)
-                  rect.lines.coming.push(line)
-                  canvas.add(line)
-                  canvas.sendToBack(line)
-                } else {
-                  rect.stroke = colorArray[types.size] 
-                  rect.fill = colorArray[types.size] 
-                  legend.push({'type': meta.value, 'color': rect.stroke})
-                  types.set(meta.value, [rect])
-                  console.log(legend)
-                  emit('update:legend', legend)
-                }
-              }
-              
-              if(meta.key === "title") {
-                const rectText = new fabric.Textbox(meta.value, { 
-                  left: rect.left, 
-                  top: rect.top,
-                  fontSize: 20,
-                  selectable: false,
-                  evented: false,
-                  width: 100 })
-                rect.title = rectText
-                canvas.add(rectText)
-              }
+      canvas.clear()
+      fabricService.drawBasket(canvas, props.basket, basketHeight, bodyWidth.value)
+      const types = new Map()
+      const legend: any[] = []
+      entities.map((entity) => {
+        fabric.Image.fromURL(entity.image, function(rect: any) {
+          if(entity.image === undefined) {
+            rect = new fabric.Rect({
+              width: 100,
+              height: 100
             })
+          } else {
+            rect.scaleToHeight(100)
+            //rect.scaleToWidth(100)
+          }
+          setRect(rect, entity)
+          rect.stroke = colorArray[0]
+          canvas.add(rect)
+
+          entity.metadata.map((meta) => {
+            if(meta.key === "type"){
+              const type = types.get(meta.value)
+              if(type){
+                rect.stroke = type[0].stroke
+                rect.fill = type[0].stroke
+                types.set(meta.value, [...type, rect])
+
+                const entity = type[type.length - 1]
+                const line = getLine(entity, rect)
+                entity.lines.going.push(line)
+                rect.lines.coming.push(line)
+                canvas.add(line)
+                canvas.sendToBack(line)
+              } else {
+                rect.stroke = colorArray[types.size] 
+                rect.fill = colorArray[types.size] 
+                legend.push({'type': meta.value, 'color': rect.stroke})
+                types.set(meta.value, [rect])
+                console.log(legend)
+                emit('update:legend', legend)
+              }
+            }
+            
+            if(meta.key === "title") {
+              const rectText = new fabric.Textbox(meta.value, { 
+                left: rect.left, 
+                top: rect.top,
+                fontSize: 20,
+                selectable: false,
+                evented: false,
+                width: 100 
+              })
+              rect.title = rectText
+              canvas.add(rectText)
+            }
           })
         })
+      })
     }
 
     const selectedSquares = (squares: any) => {
@@ -203,7 +159,7 @@ export default defineComponent({
       if(square.top < height && square.left > left && square.left < (bodyWidth.value - left)) {
         basket.push(square)
         square.set({
-          left: left + 20 + ((basket.length - 1) * 100) + ((basket.length - 1) * 20),
+          left: left + 20 + ((basket.length - 1) * 150) + ((basket.length - 1) * 20),
           top: 18,
           angle: 0
         })
@@ -213,7 +169,7 @@ export default defineComponent({
         basket.splice(squareAdded, 1)
         for(var i = 0; i < basket.length; i++) {
           basket[i].set({
-            left: left + 20 + (i * 100) + (i * 20),
+            left: left + 20 + (i * 150) + (i * 20),
             top: 18
           })
           basket[i].title?.set({'top': basket[i].top, 'left': basket[i].left})
@@ -232,8 +188,7 @@ export default defineComponent({
     )
 
     onMounted(() => {
-      canvas = new fabric.Canvas('canvas')
-      canvas.preserveObjectStacking = true // keep z-index of selected objects
+      canvas = fabricService.setupFabric()
 
       const hammer = new Hammer(canvas.upperCanvasEl, {})
       const pinch = new Hammer.Pinch()
@@ -248,8 +203,7 @@ export default defineComponent({
         }
       })
 
-      drawBasket()
-      //loadRandomSquares(canvas, 10)
+      fabricService.drawBasket(canvas, props.basket, basketHeight, bodyWidth.value)
 
       canvas.on('mouse:down', function(options: any) {
         if (options.target) {
