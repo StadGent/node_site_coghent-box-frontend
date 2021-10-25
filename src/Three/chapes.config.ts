ece154dd145b9b16ad08dc8c928d2b4ed3f70454
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2021-10-25 15:13:55 +0200

    deleted unused files

--- src/Three/chapes.config.ts
+++
@@ -1,9 +0,0 @@
-export const defaultValues = {
-  circlePoints: [
-    { angle: 25, radius: 2.5 },
-    { angle: 90, radius: 2.5 },
-    { angle: 155, radius: 2.5 },
-    { angle: 235, radius: 2.5 },
-    { angle: 310, radius: 2.5 },
-  ],
-};
