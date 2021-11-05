675b7f0687182e62196013b2eda9f117cef444d2
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2021-11-05 14:19:30 +0100

    Added empty config.json

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import 'coghent-vue-3-component-library/lib/index.css';
 import { router } from './router';
 import { environment as env } from './environments';
 
+const config = fetch('../config.json').then((r) => r.json());
+
 createApp(App)
   .use(router)
   .provide(
