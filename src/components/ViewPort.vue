b064ce239e23f4c572af653d806b3b5739c0f66f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2021-11-24 11:05:59 +0100

    #83201 the playbook will play a single story

--- src/components/ViewPort.vue
+++ src/components/ViewPort.vue
@@ -42,7 +42,7 @@ export default defineComponent({
     const stories = ref(props.stories);
     const currentStory = 1;
     const storyColor = Colors().yellow;
-    const currentFrame = 2;
+    let currentFrame = 1;
     const pause = ref(false);
     const viewport = ref(null);
     let threeSvc: ThreeService;
@@ -61,31 +61,28 @@ export default defineComponent({
 
     const buildStoryCircle = (threeSvc: ThreeService) => {
       threeSvc.state.scene.background = new Color(Colors().black);
-      threeSvc.ClearScene();
+      playBook.addToPlayBook(() => threeSvc.ClearScene());
       const circle = StoryCircle().Create(
-        'Opkomst van de\n cinema',
+        useStory().title(activeStoryData),
         CircleHelper().CreateSchema(new Vector3(0, 0, 0), 2, storyColor),
-        [currentFrame - 1, 5],
+        [currentFrame, activeStoryData.frames.length],
         'https://cdn-icons-png.flaticon.com/512/844/844994.png',
         true,
       );
-      threeSvc.AddGroupsToScene(circle);
-
       const active = CircularProgressBar().createActiveSegment(
         new Vector3(0, 0, 0),
         2.5,
         3,
-        currentFrame - 1,
+        currentFrame -1,
         storyColor,
       );
-      threeSvc.AddGroupsToScene(active.object);
       const activeFrameLine = StoryCircleItems().CreateDashedLineWithWord(
         DefaultLines().line3(active.dotSchemas[currentFrame - 1].position),
         useStory().setFrameTitles(activeStoryData)[currentFrame - 1],
       );
-      threeSvc.AddToScene(activeFrameLine.object);
+
       const progressOfFrame = StoryCircle().progressText(
-        [1, 5],
+        [currentFrame , activeStoryData.frames.length],
         new Vector3(
           activeFrameLine.endOfLine.x,
           activeFrameLine.endOfLine.y + 0.8,
@@ -93,42 +90,20 @@ export default defineComponent({
         ),
         Colors().white,
       );
-      threeSvc.AddToScene(progressOfFrame);
-      // const updated = GroupHelper().CreateGroup(circle);
-      // updated.position.set(-5,0,0);
-      // updated.scale.set(.5,0.5,0);
-      // threeSvc.AddGroupsToScene([updated]);
+
+      playBook.addToPlayBook(() => {
+        threeSvc.AddGroupsToScene(circle);
+        threeSvc.AddGroupsToScene(active.object);
+        threeSvc.AddToScene(activeFrameLine.object);
+        threeSvc.AddToScene(progressOfFrame);
+      });
+
       // threeSvc.AddToScene(Tools().Grid());
       threeSvc.state.scene.updateMatrixWorld(true);
     };
 
     const showPauseScreen = (threeSvc: ThreeService) => {
       threeSvc.ClearScene();
-    };
-
-    const addFrameOverviewToScene = (
-      threeSvc: ThreeService,
-      assets: Record<string, string>,
-    ) => {
-      threeSvc.ClearScene();
-      const overviewFrame = FrameOverview().create(assets);
-      frameAssetSchemas = overviewFrame.schemas;
-      threeSvc.AddGroupsToScene(overviewFrame.groups);
-    };
-
-    const showAsset = (asset: Asset, position: Vector3) => {
-      threeSvc.ClearScene();
-      // threeSvc.AddToScene(Tools().Grid());
-
-      const image = FrameOverview().addImage(asset, position);
-
-      threeSvc.AddToScene(image);
-
-      // setTimeout(() => {
-      //   useAsset().zoom(image as Mesh<BoxBufferGeometry, any>, threeSvc.state.height);
-      //   useAsset().setInactive(image2 as Mesh<BoxBufferGeometry, any>);
-      //   threeSvc.AddToScene(useAsset().addMetadataToZoomedImage(asset,image as Mesh<BoxBufferGeometry, any>,storyColor));
-      // }, 1000);
     };
 
     const PlayAudio = () => {
@@ -142,7 +117,6 @@ export default defineComponent({
     };
 
     const buildFrameAssetOverview = (currentFrame: number) => {
-      threeSvc.ClearScene();
       const assetsFromFrame = useAsset().getAssetsFromFrame(
         activeStoryData,
         currentFrame,
@@ -157,85 +131,58 @@ export default defineComponent({
         pos += 6;
       }
 
-      playBook.addToPlayBook(() => threeSvc.AddToScene(group));
-      // threeSvc.AddToScene(group)
-      // useAsset().zoom(group.children[2] as Mesh<BoxBufferGeometry, any>, threeSvc.state.height)
-      let highlightedImage: any;
-      playBook.addToPlayBook(() =>
-       { useAsset().zoom(
-          group.children[2] as Mesh<BoxBufferGeometry, any>,
-          threeSvc.state.height,
-        );
-        highlightedImage = useAsset().addMetadataToZoomedImage(
-        assetsFromFrame[2],
-        group.children[2] as Mesh<BoxBufferGeometry, any>,
-        storyColor,
-      );
-        threeSvc.AddToScene(highlightedImage)}
-      );
-      // threeSvc.AddToScene(highlightedImage);
-      // playBook.addToPlayBook(() => threeSvc.state.scene.remove(highlightedImage));
-      
       playBook.addToPlayBook(() => {
-        console.log('highlightedImage', highlightedImage);
-        threeSvc.state.scene.remove(highlightedImage);
-        group.children[2].scale.set(1, 1, 1);
-        group.children[2].position.set(positions[2].x, positions[2].y, positions[2].z);
+        threeSvc.ClearScene();
+        threeSvc.AddToScene(group);
       });
-      // group.children[2].scale.set(1,1,1);
-      // group.children[2].position.set(positions[2].x,positions[2].y, positions[2].z);
 
-      // group.children.forEach((asset, index) => {
-      //   playBook.addToPlayBook(() =>
-      //     {useAsset().zoom(asset as Mesh<BoxBufferGeometry, any>, threeSvc.state.height),
-      //     threeSvc.AddToScene(useAsset().addMetadataToZoomedImage(assetsFromFrame[index],asset as Mesh<BoxBufferGeometry, any>,storyColor));}
+      group.children.forEach((asset, index) => {
+        let highlightedImage: any;
+        playBook.addToPlayBook(() => {
+          useAsset().zoom(asset as Mesh<BoxBufferGeometry, any>, threeSvc.state.height);
+          highlightedImage = useAsset().addMetadataToZoomedImage(
+            assetsFromFrame[index],
+            asset as Mesh<BoxBufferGeometry, any>,
+            storyColor,
+          );
+          threeSvc.AddToScene(highlightedImage);
+        });
 
-      //   );
+        playBook.addToPlayBook(() => {
+          threeSvc.state.scene.remove(highlightedImage);
+          asset.scale.set(1, 1, 1);
+          asset.position.set(positions[index].x, positions[index].y, positions[index].z);
+          threeSvc.AddGroupsToScene(
+            HorizontalProgressBar().create(
+              new Vector3(0, -7, Layers.scene),
+              [1000, 2000, 3000],
+              5000,
+              currentFrame+1*1000,
+              storyColor,
+            ),
+          )
+        });
+      });
 
-      //   playBook.addToPlayBook(() => {
-      //     asset.scale.set(1, 1, 1);
-      //   });
-      // });
-
-      console.log(group.children[1]);
-      // useAsset().zoom(group.children[1] as Mesh<BoxBufferGeometry, any>, 600);
-      // useAsset().setInactive(group.children[0] as Mesh<BoxBufferGeometry, any>);
-      // showAsset(assetsFromFrame[0],new Vector3(0,0,0));
-      // showAsset(assetsFromFrame[1],new Vector3(5,2,0));
-      // playBook.addToPlayBook(() =>
-      //   addFrameOverviewToScene(
-      //     threeSvc,
-      //     useStory().setFrameAssets(activeStoryData, currentFrame - 1),
-      //   ),
-      // );
-      // playBook.addToPlayBook(() =>
-      //   threeSvc.AddGroupsToScene(
-      //     HorizontalProgressBar().create(
-      //       new Vector3(0, -7, Layers.scene),
-      //       [1000, 2000, 3000],
-      //       5000,
-      //       2500,
-      //       storyColor,
-      //     ),
-      //   ),
-      // );
       // playBook.addToPlayBook(() => moveSpotlight(frameAssetSchemas[0].position, 4));
       // playBook.addToPlayBook(() => moveSpotlight(frameAssetSchemas[1].position, 4));
       // playBook.addToPlayBook(() => moveSpotlight(frameAssetSchemas[2].position, 4));
       // playBook.addToPlayBook(() => moveSpotlight(frameAssetSchemas[2].position, 4));
     };
-
     const buildStory = (currentStory: number) => {
       console.log('buildStory()', storyData);
       activeStoryData = useStory().setActiveStory(storyData, currentStory - 1);
 
       spot.create(new Vector3(0, 0, Layers.scene));
       buildStoryCircle(threeSvc);
-      buildFrameAssetOverview(0);
-      // buildFrameAssetOverview(2)1
-      // buildFrameAssetOverview(3)
+      buildFrameAssetOverview(currentFrame - 1);
+      currentFrame++;
+      buildStoryCircle(threeSvc);
+      buildFrameAssetOverview(currentFrame - 1);
+      currentFrame++;
+      buildStoryCircle(threeSvc);
+      buildFrameAssetOverview(currentFrame - 1);
     };
-
     const startStory = () => {
       let current = 0;
       let time = 2;
@@ -243,12 +190,11 @@ export default defineComponent({
         console.log(Math.floor(audioSchema.audio.context.currentTime));
         if (audioHelper.DoEvent(audioSchema.audio.context.currentTime, time)) {
           playBook.getPlayBookFunctions()[current]();
-          time += 1.5;
+          time += 1;
           current++;
         }
       }, 500);
     };
-
     onMounted(() => {
       threeSvc = new ThreeService(viewport);
       audioSchema = AudioSchema(threeSvc);
@@ -259,9 +205,6 @@ export default defineComponent({
         storyData = stories.value;
         console.log('=> ACTIVE STORIES <=', stories);
         buildStory(currentStory);
-
-        // buildFrameAssetOverview(currentFrame - 1);
-        // showAsset();
         // threeSvc.AddGroupsToScene(HorizontalProgressBar().create(new Vector3(0,-7,Layers.scene),[1000,2000,3000],5000,2500,storyColor));
         startStory();
         // const pos = spot.moveTo(new Vector3(-3,2,Layers.scene), new Vector3(3,-2,Layers.scene))
