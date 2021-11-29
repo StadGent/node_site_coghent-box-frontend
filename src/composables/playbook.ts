d87a7fa3428b20c490d05ef28c8de2964138457d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2021-11-29 10:11:54 +0100

    Updated playbookHelper finished for now

--- src/composables/playbook.ts
+++ src/composables/playbook.ts
@@ -1,16 +1,20 @@
 export type PlayBookFunctions = {
-  addToPlayBook: (func: Function) => void;
-  getPlayBookFunctions: () => Array<Function>;
+  addToPlayBook: (func: Function, timestamp:number) => void;
+  getPlayBookFunctions: () => Array<{time:number,func:Function}>;
 }
 
 const PlayBook = (): PlayBookFunctions=> {
-  const playbookFunctions: Array<Function> = [];
-  const addToPlayBook = (func: Function) => {
-    playbookFunctions.push(func as Function);
+  const playbookFunctions: Array<any> = [];
+  const addToPlayBook = (func: Function, timestamp:number) => {
+    const obj = {
+      time: timestamp,
+      func: func,
+    }
+    playbookFunctions.push(obj);
   };
 
   const getPlayBookFunctions = () => {
-    return playbookFunctions;
+    return playbookFunctions.sort(ob => ob.time) as Array<{time:number,func:Function}>;
   };
 
   return {
