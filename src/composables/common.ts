ec82ceed3554beabeaa9f65ad020421c8e30e0ba
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2021-11-29 10:10:38 +0100

    Removed old get Data helper

--- src/composables/common.ts
+++ src/composables/common.ts
@@ -1,41 +1,7 @@
-import { environment as env } from '@/environments';
-import { ComponentRelation } from '@/models/GraphqlModel';
-import { GetAssetEntity } from '@/models/GraphqlQueries';
-import axios from 'axios';
-
 const Common = (): {
-  GetEntityById: (id: string) => Promise<any>;
-  GetRelationComponents: (id: string) => Promise<Array<ComponentRelation>>;
   FilterOutIdAfterSlash: (str: string) => string;
   RemoveEntersFromString: (str: string) => string;
-  ComponentIds: (components: Array<ComponentRelation>) => Array<string>;
 } => {
-  const GetEntityById = async (id: string) => {
-    try {
-      const response = await axios({
-        url: `${env.graphqlService}`,
-        method: 'post',
-        data: {
-          query: GetAssetEntity,
-          variables: {
-            id: id,
-          },
-        },
-      });
-      return response.data;
-    } catch (e) {
-      return await Promise.reject();
-    }
-  };
-
-  const GetRelationComponents = async (id: string) => {
-    try {
-      const response = await axios.get(`${env.collectionAPI}/entities/${id}/components`);
-      return response.data;
-    } catch (e) {
-      return await Promise.reject();
-    }
-  };
 
   const FilterOutIdAfterSlash = (str: string) => {
     const index = (str.indexOf('/') as number) + 1;
@@ -47,20 +13,9 @@ const Common = (): {
     return str.replace(/\n/g, '');
   };
 
-  const ComponentIds = (components: Array<ComponentRelation>) => {
-    const ids: Array<string> = [];
-    components.forEach((str) => {
-      ids.push(Common().FilterOutIdAfterSlash(str?.key as string));
-    });
-    return ids;
-  };
-
   return {
-    GetEntityById,
-    GetRelationComponents,
     FilterOutIdAfterSlash,
     RemoveEntersFromString,
-    ComponentIds,
   };
 };
 
