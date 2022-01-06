b48f96eddb6fd68612c8ad634fe7c3ec8a671bf1
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-01-06 14:13:05 +0100

    Created config.json => graphql link is get by config.json

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,19 @@ import { DefaultApolloClient } from '@vue/apollo-composable';
 
 import 'coghent-vue-3-component-library/lib/index.css';
 import { router } from './router';
-import { environment as env } from './environments';
 
-const config = fetch('../config.json').then((r) => r.json());
+async function main () {
+  const config = await fetch('../config.json').then((r) => r.json());
 
-createApp(App)
-  .use(router)
-  .provide(
-    DefaultApolloClient,
-    new ApolloClient({
-      link: createHttpLink({ uri: env.graphqlService }),
-      cache: new InMemoryCache(),
-    }),
-  )
-  .mount('#app');
+  createApp(App)
+    .use(router)
+    .provide(
+      DefaultApolloClient,
+      new ApolloClient({
+        link: createHttpLink({ uri: config.graphQlLink || '/api/graphql' }),
+        cache: new InMemoryCache(),
+      }),
+    )
+    .mount('#app');
+}
+main();
