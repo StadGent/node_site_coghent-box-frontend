ef5ecd04b0d06f0ba1f27b741f0c6c2704e9a6d4
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-01-24 14:18:35 +0100

    ANimation promises are resolved

--- src/composables/animation.ts
+++ src/composables/animation.ts
@@ -16,6 +16,7 @@ const CustomAnimation = (): {
       await Common().awaitTimeout(AnimationDefaults.timing.fadeUpdateTime);
       object.material.opacity -= step;
     }
+    Promise.resolve();
   };
 
   const fadeOutGroups = async (_groups: Array<Group>, fadeTo: number, step: number) => {
@@ -25,6 +26,7 @@ const CustomAnimation = (): {
         await CustomAnimation().fadeOut(_mesh, fadeTo, step);
       })
     })
+    Promise.resolve();
   }
 
   const fadeIn = async (object: Mesh<any, MeshBasicMaterial>, fadeTo: number, step: number) => {
@@ -32,6 +34,7 @@ const CustomAnimation = (): {
       await Common().awaitTimeout(AnimationDefaults.timing.fadeUpdateTime);
       object.material.opacity += step;
     }
+    Promise.resolve();
   };
 
   const fadeInGroups = async (_groups: Array<Group>, fadeTo: number, step: number) => {
@@ -41,6 +44,7 @@ const CustomAnimation = (): {
         await CustomAnimation().fadeIn(_mesh, fadeTo, step);
       })
     }
+    Promise.resolve();
   }
 
   const grow = async (object: Mesh<any, MeshBasicMaterial>, scaleTo: number, step: number) => {
@@ -48,6 +52,7 @@ const CustomAnimation = (): {
       await Common().awaitTimeout(AnimationDefaults.timing.scaleUpdateTime);
       object.scale.set(object.scale.x + step, object.scale.x + step, object.scale.x + step)
     }
+    Promise.resolve();
   };
 
   const shrink = async (object: Mesh<any, MeshBasicMaterial>, scaleTo: number, step: number) => {
@@ -55,6 +60,7 @@ const CustomAnimation = (): {
       await Common().awaitTimeout(AnimationDefaults.timing.scaleUpdateTime);
       Common().setScale(object, object.scale.x - step);
     }
+    Promise.resolve();
   };
 
   return {
