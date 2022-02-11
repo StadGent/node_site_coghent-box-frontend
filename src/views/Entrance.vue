8ed4e0ef17753324a42700ad96fc12f884326d8c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-02-11 12:46:38 +0100

    #89682 Done only background needs update

--- src/views/Entrance.vue
+++ src/views/Entrance.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Entrance</h1>
+  <div class="background">
+    <div class="overlay"></div>
+  </div>
+  <div>
+    <div class="image qrIcon ">
+      <img src="/images_entrance/qr_intro1.svg" alt="" />
+    </div>
+  <p class="text title">Haal hier je code om te <br>starten in de box</p>
+    <p class="text tip">je hebt een ticket nodig om in <br>te loggen.</p>
+    <div class="image dots">
+      <img src="/images_entrance/dots_intro1.svg" alt="" />
+    </div>
+    <p class="text title englishTitle">Get here your ticket to <br>start in the box</p>
+    <p class="text tip">You need a ticket to log in.</p>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -9,3 +23,66 @@ export default defineComponent({
   setup() {},
 });
 </script>
+
+<style>
+.background {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000000;
+  z-index: -2;
+}
+.overlay {
+  filter: blur(15px);
+  content: '';
+  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(45, 36, 42, 0.8)),
+    url('/background.png');
+  background-size: cover;
+  background-color: #000000;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 1080px;
+  height: 1920px;
+  z-index: -1;
+  transform: scale(1.1);
+}
+
+.image {
+width: 100%;
+display: flex;
+  justify-content: center;
+  z-index: 0;
+}
+
+.qrIcon {
+  margin-top: 500px;
+  height: 213px;
+}
+.dots {
+  margin-top: 48px;
+}
+
+.text{
+  color: #F6F5F1;
+  width: 100%;
+}
+.title{
+  margin-top: 120px;
+  font-weight: 700;
+  font-size: 54px;
+  line-height: 64px;
+  text-align: center;
+}
+.englishTitle {
+  margin-top: 24px !important;
+}
+.tip{
+  margin-top: 24px;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 54px;
+  text-align: center;
+}
+</style>
