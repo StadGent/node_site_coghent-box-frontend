8290dd14367167737e378a43f34ea1208bf07276
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-02-23 16:14:13 +0100

    #89686 Stories are set and shown with loader

--- src/components/Introscherm2.vue
+++ src/components/Introscherm2.vue
@@ -36,6 +36,27 @@
       <p class="text-3xl p-2">Welk verhaal wil je graag verder ontdekken?</p>
       <p class="font-bold text-3xl">Maak hier je keuze:</p>
       <div class="flex flex-col-4 mt-12 justify-center flex-wrap gap-12">
+        <svg
+          v-if="loading"
+          class="animate-spin -ml-1 mr-3 h-10 w-10 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
         <div
           v-for="(item, index) in stories"
           @click="nextStep(item, index)"
@@ -50,7 +71,9 @@
           >
             {{ index + 1 }}
           </div>
-          <p class="p-4 text-center w-36">{{ item.value }}</p>
+          <p class="p-4 text-center w-36">
+            {{ item.title[0] ? item.title[0].value : '' }}
+          </p>
         </div>
       </div>
     </div>
@@ -61,7 +84,7 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { CardComponent, GetActiveBoxDocument } from 'coghent-vue-3-component-library';
-import { Relation } from 'coghent-vue-3-component-library/lib/queries';
+import { Entity, Relation } from 'coghent-vue-3-component-library/lib/queries';
 import { useQuery } from '@vue/apollo-composable';
 
 export default defineComponent({
@@ -70,9 +93,11 @@ export default defineComponent({
   setup() {
     const router = useRouter();
 
-    const nextStep = (_relation: Relation, index: number) => {
-      localStorage.setItem('STORY_KEY', _relation.key);
-      // localStorage.setItem('STORY_TITLE', _relation.value as string);
+    const nextStep = (_entity: Entity, index: number) => {
+      localStorage.setItem('STORY_KEY', `entities/${_entity.id}`);
+      if (_entity.title && _entity.title[0]) {
+        localStorage.setItem('STORY_TITLE', _entity.title[0].value as string);
+      }
       localStorage.setItem('STORY_COLOR', colors[index]);
       router.push({ name: 'entrance.step3' });
     };
@@ -83,17 +108,17 @@ export default defineComponent({
       'bg-stories-blue',
       'bg-stories-yellow',
     ];
-    const stories = ref<Array<Relation>>([]);
+    const stories = ref<Array<Entity>>([]);
 
-    const { refetch: updatedStories } = useQuery(GetActiveBoxDocument);
+    const { refetch: updatedStories, loading } = useQuery(GetActiveBoxDocument);
 
     onMounted(() => {
-      updatedStories()?.then(
-        (value) => (stories.value = value.data.ActiveBox as Array<Relation>),
-      );
+      updatedStories()?.then((value) => {
+        stories.value = value.data.ActiveBox.results;
+      });
     });
 
-    return { stories, colors, nextStep };
+    return { stories, colors, nextStep, loading };
   },
 });
 </script>
