b86be1c2511ec39d26d75474680af237a89ce54e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-03-09 15:45:12 +0100

    #box update hardcoded link for printerservice useTicket

--- src/composables/useTicket.ts
+++ src/composables/useTicket.ts
@@ -7,7 +7,8 @@ const useTicket = (): {
 } => {
   const configStore = StoreFactory.get(ConfigStore)
   const print = async (_body: string) => {
-    const response = await axios.post(`${configStore.config.value.printerService}/print`, JSON.parse(_body))
+    const response = await axios.post(`http://entrance.inuits.local/print`, JSON.parse(_body))
+    // const response = await axios.post(`${configStore.config.value.printerService}/print`, JSON.parse(_body))
     console.log('print response', response)
     return response
   }
