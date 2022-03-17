a8b57d3b1dde7a616f3e38573a72dbdbd0be3e55
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-03-17 15:59:18 +0100

    #box mqtt will connect as default add ?mqtt=false to not connect

--- src/components/mqtt.vue
+++ src/components/mqtt.vue
@@ -5,7 +5,7 @@ import mqtt, { IClientSubscribeOptions } from 'mqtt';
 export default defineComponent({
   name: 'Mqtt',
   emits: ['selectStory'],
-  setup(props, {emit}) {
+  setup(props, { emit }) {
     const { host, port, endpoint, ...options } = {
       host: 'localhost',
       // host: 'mqtt.inuits.local',
@@ -25,19 +25,20 @@ export default defineComponent({
       qos: 2,
     };
 
-    client = mqtt.connect(connectUrl, options);
-    client.disconnected;
-    
+    const connectToClient = () => {
+      client = mqtt.connect(connectUrl, options);
+      client.disconnected;
 
-    client.on('connect', () => {
-      console.log('Connection succeeded!');
-    });
-    client.on('error', (error) => {
-      console.log('Connection failed', error);
-    });
+      client.on('connect', () => {
+        console.log('Connection succeeded!');
+      });
+      client.on('error', (error) => {
+        console.log('Connection failed', error);
+      });
+    };
 
     const doSubscribe = (_topic: string) => {
-      console.log('DO SCUBSCRIBE')
+      console.log('DO SCUBSCRIBE');
       const { qos } = subscription;
       client.subscribe(_topic, { qos }, (error, res) => {
         if (error) {
@@ -47,13 +48,17 @@ export default defineComponent({
       });
 
       client.on('message', (_topic, message) => {
-        console.log('topic',_topic)
-        console.log('message',message.toString())
-        emit('selectStory', {topic:_topic ,id: filterSensorOutOfTopic(_topic), msg: JSON.parse(message.toString())});
+        console.log('topic', _topic);
+        console.log('message', message.toString());
+        emit('selectStory', {
+          topic: _topic,
+          id: filterSensorOutOfTopic(_topic),
+          msg: JSON.parse(message.toString()),
+        });
       });
       client.on('reconnect', () => {
-        doSubscribe('sensors/+/#')
-      })
+        doSubscribe('sensors/+/#');
+      });
     };
 
     const filterSensorOutOfTopic = (_topic: string) => {
@@ -62,8 +67,26 @@ export default defineComponent({
       return _topic.substring(index, lastIndex);
     };
 
+    const createConnection = (_topic: string) => {
+      connectToClient();
+      doSubscribe(_topic);
+    };
+
     onMounted(() => {
-      doSubscribe('sensors/+/#');
+      const urlParams = window.location.search;
+      if (urlParams.length > 0) {
+        let params = new URLSearchParams(urlParams);
+        const mqttParamValue = params.get('mqtt');
+        console.log({ mqttParamValue });
+        if (mqttParamValue && mqttParamValue === 'true') {
+          console.log('Connect with mqtt');
+          createConnection('sensors/+/#');
+        } else {
+          console.log(`Don't Connect with mqtt`);
+        }
+      } else {
+        createConnection('sensors/+/#');
+      }
     });
   },
 });
