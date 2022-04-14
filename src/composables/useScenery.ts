832c974345a7ec8db55f1047eabec4a2fa3e6ac3
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-04-14 16:17:53 +0200

    Can scan ticket is true when welcome scene activated

--- src/composables/useScenery.ts
+++ src/composables/useScenery.ts
@@ -6,32 +6,12 @@ import ZoneService from '@/services/ZoneService'
 import Measurements from '@/Three/defaults.measurements'
 import useStartOfSession from '@/Three/playbook.startOfSession'
 import Spot from '@/Three/shapes.spotlight'
-import { Mesh, BufferGeometry, BoxGeometry, MeshBasicMaterial } from 'three'
+import { Mesh, BufferGeometry } from 'three'
 import useDMX from './useDMX'
 
 const useScenery = (): {
-  setup: () => void
   welcomeScene: () => void
 } => {
-
-  const checkAllResources = () => {
-    return globals.threeService != null && globals.spotlight != null && globals.zoneService != null
-  }
-
-  const setup = () => {
-    if (globals.zoneService) {
-      globals.spotlight = Spot().create(
-        globals.zoneService.zones[0].center,
-        Measurements().storyCircle.radius,
-      );
-    }
-
-    globals.spotlightBackground = Spot().spotLightBackground();
-    globals.spotlightBackground.material.opacity = 0;
-    globals.threeService?.AddToScene(globals.spotlight, Tags.Spotlight, 'InitialSpotlight');
-    globals.threeService?.AddToScene(globals.spotlightBackground, Tags.Spotlight, 'InitialSpotlightBackground mask');
-  }
-
   const welcomeScene = () => {
     globals.threeService?.ClearScene();
     useDMX().sequence();
@@ -49,10 +29,10 @@ const useScenery = (): {
 
     useStartOfSession(globals.threeService as ThreeService, globals.zoneService as ZoneService, globals.spotlight as Mesh<BufferGeometry, any>).showScanImage();
     stateService.changeState(FlowState.welcome);
+    stateService.canScanTicket = true
   }
 
   return {
-    setup,
     welcomeScene,
   }
 
